d2ed5de69af54d4ef87ef26cc76112f349b2a280
Igor <qgosha@list.ru>
2017-08-26 23:06:36 -0600

    d

--- main.js
+++
@@ -1,9 +0,0 @@
-(function(){
-var arr = [1,2,3,4,5,6];
-var rand = Math.round( 0 - 0.5 + Math.random() * (arr.length - 1 - 0 + 1));
-var url = "url(img/" + arr[rand] + ".jpg) no-repeat center center fixed";
-$('body').css({background:url});
-
-
-
-})();
